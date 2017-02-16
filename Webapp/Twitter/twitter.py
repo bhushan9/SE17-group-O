c13b464093a91686a3d24e50cd84a124450e6735
Zachery Thomas <zacherythomas12@gmail.com>
2017-02-16 14:04:13 -0800

    Made twitter file a class for later use.

--- Webapp/Twitter/twitter.py
+++ Webapp/Twitter/twitter.py
@@ -3,21 +3,26 @@ import json
 import sys
 
 
-#Get app and oauth keys from json file
-def get_app_keys():
-    with open('Twitter_Keys.keys') as data_file:
-	keys = json.load(data_file)
-    return keys['APP_KEY'], keys['APP_SECRET'], keys['OAUTH_TOKEN'], keys['OAUTH_TOKEN_SECRET']
+class twitter:
+
+	def __init__(self):
+    		APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET = self.get_app_keys()
+    		self.twy = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 
-#Post whatever text is passed
-def post(text):
-    APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET = get_app_keys()
-
-    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-    twitter.update_status(status = text)
+	#Get app and oauth keys from json file
+	def get_app_keys(self):
+    		with open('Twitter_Keys.keys') as data_file:
+			keys = json.load(data_file)
+    		return keys['APP_KEY'], keys['APP_SECRET'], keys['OAUTH_TOKEN'], keys['OAUTH_TOKEN_SECRET']
 
 
+	#Post whatever text is passed
+	def post(self, text):
+		self.twy.update_status(status = text)
+
+
+#Post text from twitter using arguments from command line input
 if len(sys.argv) > 1:
-    post( " ".join(sys.argv[1:]) )
+    t = twitter()
+    t.post( " ".join(sys.argv[1:]) )
