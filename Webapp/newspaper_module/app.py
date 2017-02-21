f5238ed52b6922dd49bb31ab419fff8201733011
Zachery Thomas <zacherythomas12@gmail.com>
2017-02-21 12:12:45 -0800

    Added better error checking for newspaper module

--- Webapp/newspaper_module/app.py
+++ Webapp/newspaper_module/app.py
@@ -18,21 +18,16 @@ def test(value):
 	'The Atlantic' : 'https://www.theatlantic.com/',\
 	'The Guardian' : 'https://www.theguardian.com/us' }
 
-	
-<<<<<<< HEAD
-	dailymail=newspaper.build('http://www.dailymail.co.uk/ushome/index.html')
-	if dailymail.articles:
-		article=dailymail.articles[0]
-	else:
-		article = ""
-=======
 	news_paper=newspaper.build(news_link_dict[value])
-	article=news_paper.articles[0]
->>>>>>> dbb0d9673343e5bcc187e5b955e36fa23440877e
-	article.download()
-	article.parse()
-	article.nlp()
-	return article.summary
+	try:
+		article=news_paper.articles[0]
+
+		article.download()
+		article.parse()
+		article.nlp()
+		return article.summary
+	except:
+		return 'lol sorry'
 
 @app.route('/signup', methods = ['POST','GET'])
 def signup():
@@ -48,15 +43,9 @@ def signup():
 
 @app.route("/")
 def main():
+	
 	return render_template("index.html")
     #return "Welcome!"
 
 if __name__ == "__main__":
     app.run()    
-
-
-
-
-
-
-
