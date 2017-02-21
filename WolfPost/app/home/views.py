01b674b340c800dfaeea42a79310936b653a6926
rnambis <rnambis@ncsu.edu>
2017-02-21 15:45:22 -0500

    Integrated newspaper code to login code

--- WolfPost/app/home/views.py
+++ WolfPost/app/home/views.py
@@ -1,7 +1,10 @@
 # app/home/views.py
 
-from flask import abort, render_template
+from flask import abort, render_template, request, redirect, jsonify
 from flask_login import current_user, login_required
+from sys import argv
+import newspaper
+import nltk
 
 from . import home
 
@@ -26,4 +29,34 @@ def admin_dashboard():
     if not current_user.is_admin:
     	abort(403)
     return render_template('home/admin_dashboard.html', title = "Dashboard")
+    
+def test(value):
+    news_link_dict={'Dailymail' : 'http://www.dailymail.co.uk/ushome/index.html', \
+	'BBC':'http://www.bbc.com/news', \
+	'The Economist':'http://www.economist.com/' ,\
+	'CNN' : 'http://www.cnn.com/', \
+	'New York Times' : 'https://www.nytimes.com/', \
+	'The Atlantic' : 'https://www.theatlantic.com/',\
+	'The Guardian' : 'https://www.theguardian.com/us' }
+    news_paper=newspaper.build(news_link_dict[value])
+    if news_paper.articles[0]:
+    	article=news_paper.articles[0]
+    else:
+    	article = "No article found"	
+    article.download()
+    article.parse()
+    article.nlp()
+    return article.summary
+
+@home.route('/news', methods = ['POST','GET'])
+@login_required
+def news():
+    content = " "
+    if request.method == 'POST':
+    	value = request.form['submit']
+    	return jsonify(test(value))
+    elif request.method == 'GET':
+    	pass
+    	
+    return render_template('home/news.html', title = "News", content = content)			
     	    
