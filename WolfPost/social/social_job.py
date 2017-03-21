b3bb860ef6621afcd7f1ac060dff7f49661373f0
Zachery Thomas <zacherythomas12@gmail.com>
2017-03-21 00:35:21 -0400

    Updated social_job with new API

--- WolfPost/social/social_job.py
+++ WolfPost/social/social_job.py
@@ -2,16 +2,18 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from twitter import Twitter
 import pickle
+import requests
+import json
+import newspaper
+import nltk
 
 import logging
 logging.basicConfig()
 
-import newspaper
-import nltk
 
 i = 0
 twitter = Twitter()
-
+api_key = ''
 
 def unpickle_news():
 	#Unpickle news file
@@ -22,31 +24,28 @@ def unpickle_news():
 
 def poster():
 	print 'Twitter job started...'
-	news_sources = {'Dailymail' 	: 'http://www.dailymail.co.uk/ushome/index.html',
-			'BBC'		: 'http://www.bbc.com/news',
-			'The Economist'	: 'http://www.economist.com',
-			'CNN' 		: 'http://www.cnn.com',
-			'The New York Times': 'https://www.nytimes.com',
-			'The Atlantic'	: 'https://www.theatlantic.com',
-			'The Guardian'	: 'https://www.theguardian.com/us'}
-
-	news_data = unpickle_news()
-
-	for source in news_sources:
-		try:
-			for data in news_data[source]:
-				print data['summary'] + ' ' + data['url']
-				twitter.post(data['summary'] + ' ' + data['url'])
-		except:
-			print 'something went wrong: ' + source
-
-	print 'Twitter job stopped...'
-
+	news_link_dict={'Dailymail' : 'https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=' + api_key, \
+    		'BBC':'https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=' + api_key, \
+    		'The Economist':'https://newsapi.org/v1/articles?source=the-economist&sortBy=top&apiKey=' + api_key,\
+    		'CNN' : 'https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=' + api_key, \
+    		'The New York Times' : 'https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=' + api_key, \
+    		'Bloomberg' : 'https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=' + api_key,\
+    		'The Guardian' : 'https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=top&apiKey=' + api_key }
+	
+	for key in news_link_dict:
+        	response = requests.get(news_link_dict[key])
+        	dict_response = response.json()
+        	#for i in range(len(z['articles'])):
+		text = dict_response['articles'][0]['description']
+		url =  dict_response['articles'][0]['url']
+		twitter.post(url + ' ' + text)
+		print url + ' ' + text
+		print
 
 
 def social_job():
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(poster, 'interval', minutes = 5)
+	scheduler.add_job(poster, 'interval', minutes = 10)
 	scheduler.start()
 
 
