cbe611914efd662a20154d3633a132c2890e409a
Zachery Thomas <zacherythomas12@gmail.com>
2017-03-28 18:47:43 -0400

    Testing stuff out for a sec

--- WolfPost/run.py
+++ WolfPost/run.py
@@ -3,12 +3,14 @@
 import os
 
 from app import create_app
-from news import news_job
+#from news import news_job
+#from social import social_job
 
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    news_job.news_job()
-    app.run()
+    #news_job.news_job()
+    #social_job.social_job()
+    app.run(use_reloader=False)
