8002e0a5f2427515f12a7863b05ac0c3ef82ccf0
Zachery Thomas <zacherythomas12@gmail.com>
2017-03-29 21:50:28 -0400

    Added remote db checking

--- WolfPost/app/__init__.py
+++ WolfPost/app/__init__.py
@@ -16,6 +16,11 @@ login_manager = LoginManager()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    import os
+    try:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    except:
+        print 'no external db detected'
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     Bootstrap(app)
